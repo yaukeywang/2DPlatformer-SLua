2d7d4038d71f435fe1908662e176662cdf5c2f90
Yaukey <yaukeywang@gmail.com>
2016-03-16 16:24:10 +0800

    添加注释。

--- Assets/Game/Scripts/Editor/CustomExportTypeToLua.cs
+++ Assets/Game/Scripts/Editor/CustomExportTypeToLua.cs
@@ -1,11 +1,27 @@
-﻿using UnityEngine;
+﻿/**
+ * The custom export lua type class, this is editor class.
+ *
+ * @filename  CustomExportTypeToLua.cs
+ * @copyright Copyright (c) 2015 Yaukey/yaukeywang/WangYaoqi (yaukeywang@gmail.com) all rights reserved.
+ * @license   The MIT License (MIT)
+ * @author    Yaukey
+ * @date      2016-03-16
+ */
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using SLua;
 
+// The custom export lua type class.
 public class CustomExportTypeToLua : ICustomExportPost
 {
-
+    /**
+     * Add custom class type to export.
+     * 
+     * @param LuaCodeGen.ExportGenericDelegate cAdd - The type adder delegate.
+     * @return void.
+     */
     public static void OnAddCustomClass(LuaCodeGen.ExportGenericDelegate cAdd)
     {
         // add your custom class here
@@ -19,11 +35,17 @@ public class CustomExportTypeToLua : ICustomExportPost
         cAdd(typeof(GUITexture), null);
     }
 
+    /**
+     * Add custom assembly type to export.
+     * 
+     * @param ref List<string> cList - The name list of custom assembly to load.
+     * @return void.
+     */
     public static void OnAddCustomAssembly(ref List<string> cList)
     {
         // add your custom assembly here
         // you can build a dll for 3rd library like ngui titled assembly name "NGUI", put it in Assets folder
-        // add its name into list, slua will generate all exported interface automatically for you
+        // add its name into list, slua will generate all exported interface automatically for you.
         //cList.Add("NGUI");
     }
 }
