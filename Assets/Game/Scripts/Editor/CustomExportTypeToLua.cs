3859d9af14e4b24bcc0a0c026d065c89303808b5
Yaukey <yaukeywang@gmail.com>
2016-03-20 14:59:16 +0800

    1.完全扩展 CustomExport 的每一个导出接口，更灵活的设置； 2.只导出必要的接口到 Lua，删除其他多余的导出设置。

--- Assets/Game/Scripts/Editor/CustomExportTypeToLua.cs
+++ Assets/Game/Scripts/Editor/CustomExportTypeToLua.cs
@@ -17,6 +17,20 @@ using SLua;
 public class CustomExportTypeToLua : ICustomExportPost
 {
     /**
+     * Get custom assembly to generate extension method.
+     * 
+     * @param out List<string> cList - The result out list.
+     * @return void.
+     */
+    public static void OnGetAssemblyToGenerateExtensionMethod(out List<string> cList)
+    {
+        cList = new List<string>
+        {
+            
+        };
+    }
+
+    /**
      * Add custom class type to export.
      * 
      * @param LuaCodeGen.ExportGenericDelegate cAdd - The type adder delegate.
@@ -30,9 +44,6 @@ public class CustomExportTypeToLua : ICustomExportPost
         // typename used for simplify generic type name or rename, like List<int> named to "ListInt", if not a generic type keep typename as null or rename as new type name.
         //cAdd(typeof(string), "String");
 
-        cAdd(typeof(GUIElement), null);
-        cAdd(typeof(GUIText), null);
-        cAdd(typeof(GUITexture), null);
         cAdd(typeof(YwDebug), null);
         cAdd(typeof(YwLuaMonoBehaviourBase), null);
         cAdd(typeof(YwLuaMonoBehaviour), null);
@@ -52,5 +63,98 @@ public class CustomExportTypeToLua : ICustomExportPost
         // you can build a dll for 3rd library like ngui titled assembly name "NGUI", put it in Assets folder
         // add its name into list, slua will generate all exported interface automatically for you.
         //cList.Add("NGUI");
+    }
+
+    /**
+     * Get custom namespace to export.
+     * 
+     * @param void.
+     * @return HashSet<string> - The set of the string.
+     */
+    public static HashSet<string> OnAddCustomNamespace()
+    {
+        return new HashSet<string>
+        {
+            
+        };
+    }
+
+    // The filter list.
+    public static List<string> FunctionFilterList = new List<string>()
+    {
+        
+    };
+
+    /**
+     * Get the use list for exporting unity engine.
+     * 
+     * @param out List<string> list - The name of exporting list.
+     * @return void.
+     */
+    public static void OnGetUseList(out List<string> list)
+    {
+        list = new List<string>
+        {
+            "UnityEngine.Object",
+            "UnityEngine.GameObject",
+            "UnityEngine.Component",
+            "UnityEngine.Behaviour",
+            "UnityEngine.MonoBehaviour",
+            "UnityEngine.Transform",
+            "UnityEngine.Camera",
+            "UnityEngine.GUIElement",
+            "UnityEngine.GUIText",
+            "UnityEngine.GUITexture",
+            "UnityEngine.Animator",
+            "UnityEngine.Color",
+            "UnityEngine.Material",
+            "UnityEngine.Renderer",
+            "UnityEngine.SpriteRenderer",
+            "UnityEngine.ParticleSystemRenderer",
+            "UnityEngine.ParticleSystem",
+            "UnityEngine.Sprite",
+            "UnityEngine.AudioSource",
+            "UnityEngine.AudioClip",
+            "UnityEngine.Time",
+            "UnityEngine.WaitForSeconds",
+            "UnityEngine.Vector2",
+            "UnityEngine.Vector3",
+            "UnityEngine.Vector4",
+            "UnityEngine.Quaternion",
+            "UnityEngine.Physics2D",
+            "UnityEngine.RaycastHit2D",
+            "UnityEngine.Rigidbody2D",
+            "UnityEngine.RigidbodyConstraints2D",
+            "UnityEngine.Collision2D",
+            "UnityEngine.Collider2D",
+            "UnityEngine.BoxCollider2D",
+            "UnityEngine.CircleCollider2D",
+            "UnityEngine.PolygonCollider2D",
+            "UnityEngine.EdgeCollider2D",
+            "UnityEngine.Input",
+            "UnityEngine.KeyCode",
+            "UnityEngine.SceneManagement",
+            "UnityEngine.SceneManagement.SceneManager",
+            "UnityEngine.SceneManagement.Scene",
+            "UnityEngine.Debug",
+            "UnityEngine.LayerMask",
+            "UnityEngine.Mathf",
+            "UnityEngine.Random",
+            "UnityEngine.Application",
+        };
+    }
+
+    /**
+     * Get the useless list for exporting unity engine.
+     * 
+     * @param out List<string> list - The name of useless list.
+     * @return void.
+     */
+    public static void OnGetNoUseList(out List<string> list)
+    {
+        list = new List<string>
+        {
+            
+        };
     }
 }
--- Assets/Slua/LuaObject/Custom/LuaDelegate_UnityEngine_Events_UnityAction.cs
+++ Assets/Slua/LuaObject/Custom/LuaDelegate_UnityEngine_Events_UnityAction.cs
@@ -9,7 +9,7 @@ namespace SLua
     public partial class LuaDelegation : LuaObject
     {
 
-        static internal int checkDelegate(IntPtr l,int p,out UnityEngine.Application.AdvertisingIdentifierCallback ua) {
+        static internal int checkDelegate(IntPtr l,int p,out UnityEngine.Events.UnityAction ua) {
             int op = extractFunction(l,p);
 			if(LuaDLL.lua_isnil(l,p)) {
 				ua=null;
@@ -17,27 +17,24 @@ namespace SLua
 			}
             else if (LuaDLL.lua_isuserdata(l, p)==1)
             {
-                ua = (UnityEngine.Application.AdvertisingIdentifierCallback)checkObj(l, p);
+                ua = (UnityEngine.Events.UnityAction)checkObj(l, p);
                 return op;
             }
             LuaDelegate ld;
             checkType(l, -1, out ld);
             if(ld.d!=null)
             {
-                ua = (UnityEngine.Application.AdvertisingIdentifierCallback)ld.d;
+                ua = (UnityEngine.Events.UnityAction)ld.d;
                 return op;
             }
 			LuaDLL.lua_pop(l,1);
 			
 			l = LuaState.get(l).L;
-            ua = (string a1,bool a2,string a3) =>
+            ua = () =>
             {
                 int error = pushTry(l);
 
-				pushValue(l,a1);
-				pushValue(l,a2);
-				pushValue(l,a3);
-				ld.pcall(3, error);
+				ld.pcall(0, error);
 				LuaDLL.lua_settop(l, error-1);
 			};
 			ld.d=ua;
